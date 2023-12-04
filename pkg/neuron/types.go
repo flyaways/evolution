be689f73e3c794e695b7918a39818d0ff2b4bb88
flyaways <meagleglass@gmail.com>
2023-12-04 16:39:10 +0800

    init

--- pkg/neuron/types.go
+++
@@ -1,9 +0,0 @@
-package neuron
-
-// Neuron 结构体
-type Neuron struct {
-	Weights []float64 `yaml:"weights" toml:"weights" json:"weights"`
-	Bias    float64   `yaml:"bias" toml:"bias" json:"bias"`
-	Delta   float64   `yaml:"delta" toml:"delta" json:"delta"`
-	Output  float64   `yaml:"output" toml:"output" json:"output"`
-}
